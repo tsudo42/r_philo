658d9dafc4a58ae1a5b349f222547cd6ed059eef
tsudo42 <91622558+tsudo42@users.noreply.github.com>
2022-08-24 09:21:30 +0900

    Fix: norm

--- srcs/philo/monitor.c
+++ srcs/philo/monitor.c
@@ -12,30 +12,38 @@
 
 #include "philo.h"
 
+static int	check(t_philo *philo)
+{
+	long	time;
+
+	sem_wait(philo->sem->sem_monitor);
+	time = get_time();
+	if (philo->last_eat < time && philo->starve_time < time)
+	{
+		print_state(philo, DIED);
+		philo->state = STARVED;
+		return (1);
+	}
+	if (philo->arg->num_to_eat > 0 && \
+		philo->arg->num_to_eat < philo->eat_count)
+	{
+		philo->state = END;
+		return (1);
+	}
+	sem_post(philo->sem->sem_monitor);
+	return (0);
+}
+
 void	*monitor(void *ptr)
 {
 	t_philo	*philo;
-	long	time;
 
 	philo = ptr;
 	usleep(philo->arg->time_to_die * 500);
 	while (1)
 	{
-		sem_wait(philo->sem->sem_monitor);
-		time = get_time();
-		if (philo->last_eat < time && philo->starve_time < time)
-		{
-			print_state(philo, DIED);
-			philo->state = STARVED;
+		if (check(philo))
 			break ;
-		}
-		if (philo->arg->num_to_eat > 0 && \
-			philo->arg->num_to_eat < philo->eat_count)
-		{
-			philo->state = END;
-			break ;
-		}
-		sem_post(philo->sem->sem_monitor);
 		usleep(5000);
 	}
 	sem_post(philo->sem->sem_state);
